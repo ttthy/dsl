6fe6a73ba8209fb28deb512ee60ac9ca6578bc78
ttthy <thy2512@outlook.com>
2020-08-23 17:12:32 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# dsl
+# DSL
 Discrimination of Similar Languages
+
+
+## Environment
+
+```shell
+pip install -r requirements.txt
+```
+
+## Dataset & Preprocessing
+Download the dataset from [DSLLCC](http://ttg.uni-saarland.de/resources/DSLCC/)
+We use [DSLCC v4.0](http://scholar.harvard.edu/files/malmasi/files/dslcc4.zip)
+
+You should change the directory in the file
+```
+python preprocessing.py 
+```
+
+## Usage
+
+SVM baselines
+```shell
+python -m svm.svm [charngram/2step]
+```
+
+Neural baselines
+```shell
+python -m charcnn.main --save_dir [checkpoints/temp] --model [bow/cnn/multicnn]
+```
--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,10 @@
 from collections import Counter
-import spacy
+from nltk.tokenize import word_tokenize
 import argparse
 from tqdm import tqdm
 
 
-def tokenise_raw_data(infile, outfile, spacy_model):
+def tokenise_raw_data(infile, outfile):
     """ Load data from file """
     with open(infile, 'r') as f, open(outfile, 'w') as w:
         for line in tqdm(f):
@@ -12,19 +12,18 @@ def tokenise_raw_data(infile, outfile, spacy_model):
             try:
                 x, y = line.split('\t')
                 x, y = x.strip(), y.strip()
-                x = " ".join([tok.text for tok in spacy_model(x)])
+                x = " ".join(word_tokenize(x))
                 w.write("{}\t{}\n".format(x, y))
             except ValueError:
                 x = line.strip()
-                x = " ".join([tok.text for tok in spacy_model(x)])
+                x = " ".join(word_tokenize(x))
                 w.write("{}\n".format(x))
 
 
 if __name__ == "__main__":
-    spacy_model = spacy.load("pt_core_news_sm")
     partition = ["TRAIN", "DEV", "TEST-UNLABELLED"]
     abs_path = "data/DSL-"
     for p in partition:
         infilepath = "{}{}.txt".format(abs_path, p)
         outfilepath = "{}{}.txt.tok".format(abs_path, p)
-        tokenise_raw_data(infilepath, outfilepath, spacy_model)
+        tokenise_raw_data(infilepath, outfilepath)
